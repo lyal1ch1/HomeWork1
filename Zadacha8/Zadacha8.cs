a9cf81ead447689ef36951d71613dd3527f62acf
lyal1ch1 <106307360+lyal1ch1@users.noreply.github.com>
2022-12-01 14:55:38 +0300

    Задача № 8

---
+++ Zadacha8/Zadacha8.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число от 1 до N :");
+string num = Console.ReadLine()!;
+
+double num1 = double.Parse(num);
+Console.Clear();
+if (num1 < 1)
+{
+ Console.WriteLine("Введите число больше 1 !!!");   
+}
+else
+{
+    Console.WriteLine("Ваши числа: ");
+}
+for (int i = 2; i <= num1; i+=2)
+{
+    Console.Write( i + " ");
+}
+

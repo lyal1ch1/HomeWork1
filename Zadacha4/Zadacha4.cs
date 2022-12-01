f8982825ff5291afc416cee4de93db7cb0a28c6b
lyal1ch1 <106307360+lyal1ch1@users.noreply.github.com>
2022-12-01 12:58:35 +0300

    Задача № 4

---
+++ Zadacha4/Zadacha4.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("Введите первое число:");
+string number1 = Console.ReadLine()!;
+
+Console.WriteLine("Введите второе число:");
+string number2 = Console.ReadLine()!;
+
+Console.WriteLine("Введите третье число:");
+string number3 = Console.ReadLine()!;
+
+double Number_1 = double.Parse(number1);
+double Number_2 = double.Parse(number2);
+double Number_3 = double.Parse(number3);
+
+if (Number_1 > Number_2)
+{
+    if (Number_1 > Number_3)
+    {
+        Console.WriteLine("Max=" + Number_1);
+    }
+}
+
+if (Number_2 > Number_3)
+{
+    if (Number_2 > Number_1)
+    {
+        Console.WriteLine("Max=" + Number_2);
+    }
+}
+
+if (Number_3 > Number_2)
+{
+    if (Number_3 > Number_1)
+    {
+        Console.WriteLine("Max=" + Number_3);
+    }
+}
+
+
+if (Number_1 == Number_2 )
+{
+    if (Number_2 == Number_3 )
+    Console.WriteLine("Числа равны");
+}

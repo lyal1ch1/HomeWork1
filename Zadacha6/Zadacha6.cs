e31fba40b07aa87ac8bfec5bd44e0443ed376292
lyal1ch1 <106307360+lyal1ch1@users.noreply.github.com>
2022-12-01 13:11:55 +0300

    Задача № 6

---
+++ Zadacha6/Zadacha6.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число:");
+string num = Console.ReadLine()!;
+double num1 = double.Parse(num);
+
+if (num1 % 2 == 0)
+{
+ Console.WriteLine (num1 +"- четное число!");
+
+}
+
+else
+{
+Console.WriteLine (num1 +"- не четное число!");
+    
+}
